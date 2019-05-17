2b633cd5df70e0a45d8136b2f29b320a857667aa
btronquoy <b.tronquoy@gmail.com>
2019-05-17 13:26:54 +0200

    feat(Profile Public): user profile by id

--- src/views/User/ProfilePublic.vue
+++ src/views/User/ProfilePublic.vue
@@ -2,11 +2,15 @@
 {
   "fr": {
     "viewTitle": "Profile publique",
-    "viewDescription": "Profile de: "
+    "viewDescription": "Profile de: ",
+    "errUserNotSet": "Cet utilisateur n'a pas encore défini son pseudo",
+    "errAvatarNotSet": "Cet utilisateur n'a pas encore défini son avatar"
   },
   "en": {
     "viewTitle": "Public profile",
-    "viewDescription": "This is the profile of:"
+    "viewDescription": "This is the profile of:",
+    "errUserNotSet": "This user has not set his username yet",
+    "errAvatarNotSet": "This user has not set his avatar"
   }
 }
 </i18n>
@@ -17,7 +21,12 @@
       <h1>{{ $t('viewTitle') }}</h1>
       <v-layout row>
       <v-flex xs12 md6>
-        <p>{{ $t('viewDescription') }} <b>{{ $route.params.scUsername }}</b></p>
+        <p>{{ $t('viewDescription') }} <b>{{ user.scUsername }}</b></p>
+          <ul>
+            <li>Username: {{ user.scUsername ? user.scUsername : $t('errUserNotSet') }}</li>
+            <li>Avatar: {{ user.avatar ? user.avatar : $t('errAvatarNotSet') }}</li>
+            <li>RSI profile: {{ user.scUsername ? rsiUrl + user.scUsername : $t('errUserNotSet') }}</li>
+          </ul>
       </v-flex>
       </v-layout>
     </v-container>
@@ -25,10 +34,20 @@
 </template>
 
 <script>
+import { db } from '@/fb'
 export default {
   data () {
     return {
+      user: {},
+      rsiUrl: 'https://robertsspaceindustries.com/citizens/'
     }
+  },
+  methods: {
+  },
+  firestore () {
+  return {
+    user: db.collection('users').doc(this.$route.params.id) 
   }
+}
 }
 </script>
